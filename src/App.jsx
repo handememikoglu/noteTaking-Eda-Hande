d6a056927557ac59b86e5c5dd11068df5c715e12
Edanur Korkmaz <edanuurkorkmaz@gmail.com>
2025-03-19 19:06:51 +0300

    archive eklendi.

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,61 @@ import HomePage from "./components/HomePage";
 import Search from "./components/Search";
 import Archive from "./components/Archive";
 import Tags from "./components/Tags";
-import Settings from "./components/Settings"
+import Settings from "./components/Settings";
 
 function App() {
-    const [page,setPage] = useState(window.location.pathname);
+    const [page, setPage] = useState(window.location.pathname);
+    const [notes, setNotes] = useState(
+        localStorage.getItem("notes")
+            ? JSON.parse(localStorage.getItem("notes"))
+            : []
+    );
+    const [archivedNotes, setArchivedNotes] = useState(
+        localStorage.getItem("archivedNotes")
+            ? JSON.parse(localStorage.getItem("archivedNotes"))
+            : []
+    );
+    useEffect(() => {
+        console.log(notes);
+        console.log(archivedNotes);
 
-    useEffect(() =>{
+        localStorage.setItem("notes", JSON.stringify(notes));
+        localStorage.setItem("archivedNotes", JSON.stringify(archivedNotes));
+    }, [notes, archivedNotes]);
+
+    const archiveNote = (id) => {
+        const noteToArchive = notes.find((note) => note.id === id);
+
+        if (noteToArchive) {
+            setNotes(notes.filter((note) => note.id !== id));
+            setArchivedNotes([...archivedNotes, noteToArchive]);
+        }
+    };
+
+    useEffect(() => {
         const handlePopState = () => setPage(window.location.pathname);
-        window.addEventListener('popstate',handlePopState);
+        window.addEventListener("popstate", handlePopState);
 
-        return () => window.removeEventListener('popstate',handlePopState);
-    },[])
+        return () => window.removeEventListener("popstate", handlePopState);
+    }, []);
 
     const navigate = (path) => {
-        window.history.pushState({},"",path);
+        window.history.pushState({}, "", path);
         setPage(path);
-    }
+    };
     return (
         <div className="flex">
             <Sidebar navigate={navigate} currentPage={page} />
             <div className="flex-1 p-4">
-                {page === "/" && <HomePage />}
+                {page === "/" && (
+                    <HomePage
+                        notes={notes}
+                        setNotes={setNotes}
+                        archiveNote={archiveNote}
+                    />
+                )}
                 {page === "/Arama" && <Search />}
-                {page === "/Arsiv" && <Archive />}
+                {page === "/Arsiv" && <Archive archivedNotes={archivedNotes} />}
                 {page === "/Etiketler" && <Tags />}
                 {page === "/Ayarlar" && <Settings />}
             </div>
--- src/components/Archive.jsx
+++ src/components/Archive.jsx
@@ -1,3 +1,31 @@
-export default function Archive({ notes, noteContent, selectedTags }) {
-    return <div>{console.log("arşivaaaaaaaaaaaaaaaaaaa")}</div>;
+export default function Archive({ archivedNotes }) {
+    return (
+        <div className="mt-8">
+            <h3 className="font-semibold">Archived Notes</h3>
+            <div className="mt-4">
+                {archivedNotes.map((anote) => (
+                    <div
+                        key={anote.id}
+                        className=" p-4  mb-4 border-b-1 border-gray-400"
+                    >
+                        <p>{anote.content}</p>
+                        <div className="flex gap-2 mt-2">
+                            {anote.tags.map((tag) => (
+                                <span
+                                    key={tag}
+                                    className="px-3 py-1 bg-gray-300 text-black rounded-md"
+                                >
+                                    {tag}
+                                </span>
+                            ))}
+                        </div>
+
+                        <p className="mt-2 text-sm text-gray-500">
+                            {anote.date}
+                        </p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
